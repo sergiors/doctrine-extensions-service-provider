5a2314b1e94ae498d6e90da1b89eb48a17c70026
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2016-02-24 21:09:14 -0300

    Add skeleton to tests

---
+++ tests/Provider/DoctrineExtensionsServiceProviderTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sergiors\Silex\Tests\Provider;
+
+use Sergiors\Silex\Provider\DoctrineExtensionsServiceProvider;
+
+class DoctrineExtensionsServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+
+}
